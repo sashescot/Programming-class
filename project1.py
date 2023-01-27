2962e4e511e3c127d4b35b7189eafb04710edc05
sashescot <123759909+sashescot@users.noreply.github.com>
2023-01-27 17:54:22 +0000

    added project 1

---
+++ project1.py
@@ -0,0 +1,20 @@
+Rect (0, 0, 400, 400, fill='darkTurquoise') #background color
+
+Line (200, 400, 200, 280, fill="green", lineWidth=4) #stem of flower
+Oval(200, 230, 50, 70, fill='white') #flower petal
+Oval(240, 250, 50, 70, fill='white', rotateAngle= 60)#flower petal 
+Oval(200, 230, 50, 70, fill='white') #flower petal
+Oval(245, 300, 50, 70, fill='white', rotateAngle= 100) #flower petal
+Oval(160, 250, 50, 70, fill='white', rotateAngle= 125) #flower petal
+Oval(165, 300, 50, 70, fill='white', rotateAngle= 80) #flower petal
+Circle(200, 280, 25, fill='gold') #flower center
+
+Oval(120, 50, 60, 50, fill='gainsboro') #cloud1
+Oval(140, 25, 60, 50, fill='gainsboro') #cloud1
+Oval(160, 60, 60, 50, fill='gainsboro') #cloud1
+
+Oval(280, 40, 60, 50, fill='gainsboro') #cloud2
+Oval(300, 65, 60, 50, fill='gainsboro') #cloud2
+Oval(260, 55, 60, 50, fill='gainsboro') #cloud2
+
+Circle(40, 40, 75, fill='lemonChiffon') #sun
